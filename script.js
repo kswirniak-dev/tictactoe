8c8d044d8fcfdd68f538ecc2d23d4c37543e757a
kswirniak <swirniak@gmail.com>
2021-04-02 22:23:54 +0200

    added css circle class

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 toggleCross = (element) => {
-    if (!element.childNodes[0].classList.contains("cross"))
+    if (element.childNodes[0].classList.length === 0)
     {        
         element.childNodes[0].classList.add("cross");
     }
     else
     {
-        element.childNodes[0].classList.remove("cross")
+        element.childNodes[0].classList.remove(element.childNodes[0].classList[0])
+        if (element.childNodes[0].childNodes.length > 0) {
+            element.childNodes[0].removeChild(element.childNodes[0].childNodes[0]);
+        }
+        
     }
 }
