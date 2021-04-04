210d8429c6a847010c45c89204b0d2c1aa499c91
kswirniak <swirniak@gmail.com>
2021-04-04 12:56:00 +0200

    refactored, createMessageBox function added

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let turn = "cross";
+let turn = 'cross';
 const WINNING_COMBINATIONS = [ [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
 
 makeTurn = (element) => {
-    const alertBox = document.getElementById("warning-cell-taken");
+    const messageBox = document.getElementById('alert-message');
     if (!isCellEmpty(element)){
-        alertBox.classList.remove('hidden');
-        return null;
+        messageBox = createMessageBox('warning', 'To pole jest zajęte. Wybierz inne');
     }
     markCell(element);
     makeElementInvinsible(alertBox);
-    console.log(checkforVictory());
+    if (checkforVictory()) {
+        messageBox = createMessageBox('gameover', 'Koniec gry. Zwycięzca: ' + turn);
+        }
+    }
     turn = changeTurn();
 }
 
@@ -51,7 +53,23 @@ changeTurn = () => turn === "circle" ? "cross" : "circle";
 
 makeElementInvinsible = (element) => element.classList.add('hidden');
 
-checkforVictory = () => WINNING_COMBINATIONS.some( combination => combination.every(number => [].slice.call(document.getElementsByClassName(turn))
+checkforVictory = () => WINNING_COMBINATIONS.some(combination => combination.every(number => [].slice.call(document.getElementsByClassName(turn))
         .map(cell => Number(cell.parentNode.id))
         .includes(number)))
+        
 
+createMessageBox = (className, text) => {
+    messageBox = document.createElement(div);
+    
+    alertBox.classList.remove('hidden');
+    alertBox.classList.remove('gameover');
+    alertBox.classList.remove('warning');
+
+    alertBox.classList.add(className);
+    const p = document.createElement("p")
+    text = document.createTextNode(text)
+    p.appendChild(text);
+    alertBox.appendChild(p);
+    
+    return alertBox;
+}
