06f114be5327a3464ef049adf14766ebf5822d22
kswirniak <swirniak@gmail.com>
2021-04-04 13:49:02 +0200

    improved structure of displaying messages

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ let turn = 'cross';
 const WINNING_COMBINATIONS = [ [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
 
 makeTurn = (element) => {
-    const messageBox = document.getElementById('alert-message');
+    let messageBox = document.getElementById('message-box');
+    const page = document.getElementById('page');   
     if (!isCellEmpty(element)){
-        messageBox = createMessageBox('warning', 'To pole jest zajęte. Wybierz inne');
+        messageBox = createMessageBox('warning', 'To pole jest zajęte. Wybierz inne.');
+        page.insertBefore(messageBox, page.childNodes[0]);        
+        return null;     
+    }
+    if (page.firstChild.id === 'message-box'){
+        page.removeChild(page.firstChild);
     }
     markCell(element);
-    makeElementInvinsible(alertBox);
     if (checkforVictory()) {
         messageBox = createMessageBox('gameover', 'Koniec gry. Zwycięzca: ' + turn);
+        page.insertBefore(messageBox, page.childNodes[0])
         }
-    }
     turn = changeTurn();
 }
 
 markCell = (element) => {
-    if (turn === "circle") {
+    if (turn === 'circle') {
         element.appendChild(createCircleNode(element.childNodes[0]));
         element.removeChild(element.firstChild);
     }
-    else if (turn === "cross"){
+    else if (turn === 'cross'){
         element.appendChild(createCrossNode(element.childNodes[0]));
         element.removeChild(element.firstChild);
     }
@@ -29,16 +34,16 @@ markCell = (element) => {
 
 createCircleNode = (divNode) => {
     const resultNode = divNode.cloneNode();
-    resultNode.classList.add("circle");
-    const innerDiv = document.createElement("div");
-    innerDiv.classList.add("innercircle");
+    resultNode.classList.add('circle');
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('innercircle');
     resultNode.appendChild(innerDiv);
     return resultNode;
 }
 
 createCrossNode = (divNode) => {
     const resultNode = divNode.cloneNode();
-    resultNode.classList.add("cross");
+    resultNode.classList.add('cross');
     return resultNode;
 }
 
@@ -49,9 +54,9 @@ isCellEmpty = (element) => {
     return true;
 }
 
-changeTurn = () => turn === "circle" ? "cross" : "circle";
+changeTurn = () => turn === 'circle' ? 'cross' : 'circle';
 
-makeElementInvinsible = (element) => element.classList.add('hidden');
+hideElement = (element) => element.classList.add('hidden');
 
 checkforVictory = () => WINNING_COMBINATIONS.some(combination => combination.every(number => [].slice.call(document.getElementsByClassName(turn))
         .map(cell => Number(cell.parentNode.id))
@@ -59,17 +64,22 @@ checkforVictory = () => WINNING_COMBINATIONS.some(combination => combination.eve
         
 
 createMessageBox = (className, text) => {
-    messageBox = document.createElement(div);
-    
-    alertBox.classList.remove('hidden');
-    alertBox.classList.remove('gameover');
-    alertBox.classList.remove('warning');
-
-    alertBox.classList.add(className);
+    messageBox = document.createElement('div');
+    messageBox.setAttribute('id', 'message-box');
+    messageBox.classList.add('alert');
+    messageBox.classList.add(className);
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('closebtn');
+    closeButton.onclick = function(event) {
+        this.parentElement.classList.add('hidden');
+    }
+    const crossButton =  document.createTextNode('×');
+    closeButton.appendChild(crossButton);
     const p = document.createElement("p")
     text = document.createTextNode(text)
     p.appendChild(text);
-    alertBox.appendChild(p);
+    messageBox.appendChild(closeButton);
+    messageBox.appendChild(p);
     
-    return alertBox;
+    return messageBox;
 }
